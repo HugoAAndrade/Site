bbdfb09636709936afe8131663f7d0a86cd44879
Hugo Andrade <hugoalvesandrade@hotmail.com>
2023-06-18 23:12:22 -0300

    Ajuste pagina sobre e css

--- src/app/sobre-mim/page.js
+++ src/app/sobre-mim/page.js
@@ -1,9 +1,6 @@
-import styles from "./page.module.scss";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import Principal from "@/components/Principal/Principal";
-import Carrossel from "@/components/Carrossel/Carrossel";
-import Cards from "@/components/Cards/Cards";
 import Contato from "@/components/Contato/Contato";
 
 export default function Sobre() {

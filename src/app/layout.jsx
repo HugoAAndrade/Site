04651789fef56d6f3ce23bda2e205cd0ed83fc0a
Hugo Andrade <hugoalvesandrade@hotmail.com>
2023-07-15 22:22:04 -0300

    Add vercel analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import "./globals.scss";
 import { Outfit } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br" className="dark">
-      <body className={outfit.className}>{children}</body>
+      <body className={outfit.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }

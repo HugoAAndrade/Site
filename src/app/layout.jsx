2df1fb6fb99a4dc6bae460f5ddedb37c9ef3ed49
Hugo Andrade <85207583+HugoAAndrade@users.noreply.github.com>
2025-08-06 13:39:18 -0300

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br" className="dark">
+    <html lang="pt-br" className="light">
       <body className={outfit.className}>
         {children}
         <Analytics />
